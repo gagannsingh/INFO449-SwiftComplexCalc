df446704be67a3c249aa7815cf9b6f35046dee69
gagannsingh <122052524+gagannsingh@users.noreply.github.com>
2024-04-06 19:56:30 -0700

    Update Contents.swift

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -31,8 +31,14 @@ class Calculator {
     func add(lhs: Int, rhs: Int) -> Int {
         return lhs + rhs
     }
+    func add(_ numbers: [Int]) -> Int {
+        return numbers.reduce(0, +)
+    }
     func subtract(lhs: Int, rhs: Int) -> Int {
         return lhs - rhs
+    }
+    func multiply(_ numbers: [Int]) -> Int {
+        return numbers.reduce(1, *)
     }
     func multiply(lhs: Int, rhs: Int) -> Int {
         return lhs * rhs
@@ -43,7 +49,14 @@ class Calculator {
     func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int {
         return op(lhs, rhs)
     }
-    
+    func count(_ numbers: [Int]) -> Int {
+        return numbers.count
+    }
+    func avg(_ numbers: [Int]) -> Int {
+        let total = numbers.reduce(0, +)
+        let total_numbers = numbers.count
+        return total/total_numbers
+    }
     
 }
 
@@ -70,26 +83,26 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
-//calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
+calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
 //    // This is the second, more terse, style; either works
-//
-//calc.add([1, 2, 3, 4, 5]) == 15
-//calc.multiply([1, 2, 3, 4, 5]) == 120
-//calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8
-//calc.count([]) == 0
-//calc.avg([2, 2, 2, 2, 2, 2]) == 2
-//calc.avg([1, 2, 3, 4, 5]) == 3
-//calc.avg([1]) == 1
-//
-//calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
-//    // this is (((0 op 1) op 2) op 3)
+
+calc.add([1, 2, 3, 4, 5]) == 15
+calc.multiply([1, 2, 3, 4, 5]) == 120
+calc.count([1, 2, 3, 4, 5, 6, 7, 8]) == 8
+calc.count([]) == 0
+calc.avg([2, 2, 2, 2, 2, 2]) == 2
+calc.avg([1, 2, 3, 4, 5]) == 3
+calc.avg([1]) == 1
+
+calc.mathOp(args: [1, 2, 3], beg: 0, op: { $0 + $1 }) == 6
+    // this is (((0 op 1) op 2) op 3)
 //calc.mathOp(args: [1, 2, 3, 4, 5], beg: 0, op: { $0 + $1 }) == 15
-//    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
+    // this is (((((0 op 1) op 2) op 3) op 4) op 5)
 //calc.mathOp(args: [1, 1, 1, 1, 1], beg: 1, op: { $0 * $1 }) == 1
-//    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
-//
+    // this is (((((1 op 1) op 1) op 1) op 1) op 1)
+
 //let p1 = (5, 5)
 //let p2 = (12, -27)
 //let p3 = (-4, 4)
@@ -98,7 +111,7 @@ calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (
 //calc.subtract(lhs: p1, rhs: p2) == (-7, 32)
 //calc.add(lhs: p4, rhs: p4) == (0, 0)
 //calc.add(lhs: p3, rhs: p4) == (-4, 4)
-//
+
 //let pd1 = ["x": 5, "y": 5]
 //let pd2 = ["x": -4, "y": 4]
 //calc.add(lhs: pd1, rhs: pd2) == ["x": 1, "y": 9]
